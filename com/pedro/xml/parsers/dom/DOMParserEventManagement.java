bc36006777d4cb2ae8feaee896c3e7b2d5bd7017
albertobolina <60327260+albertobolina@users.noreply.github.com>
2024-04-24 17:07:12 +0000

    adicionado java dom parser

---
+++ com/pedro/xml/parsers/dom/DOMParserEventManagement.java
@@ -0,0 +1,18 @@
+package com.pedro.xml.parsers.dom;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class DOMParserEventManagement {
+
+    public static void main(String[] args) throws ParserConfigurationException {
+        
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();        
+        DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+        
+    }
+
+}
