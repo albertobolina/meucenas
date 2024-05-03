83a0f974e41a537b3c370e4efb926747b77a3c2f
albertobolina <60327260+albertobolina@users.noreply.github.com>
2024-05-03 08:29:14 +0000

    mais 1

--- com/pedro/xml/parsers/dom/DOMParserEventManagement.java
+++ com/pedro/xml/parsers/dom/DOMParserEventManagement.java
@@ -7,7 +7,11 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
+
+import com.pedro.xml.parsers.dto.*;
 
 public class DOMParserEventManagement {
 
@@ -17,6 +21,20 @@ public class DOMParserEventManagement {
         DocumentBuilder documentBuilder = factory.newDocumentBuilder();
         Document documento = documentBuilder.parse(ClassLoader.getSystemResourceAsStream("xml/evento.xml"));
         
+        Evento novoEvento = new Evento();
+
+        // a lista vai conter todos os elementos cujo nome de tag seja nome, que estejam no nivel inicial
+        NodeList nomeEventoList = documento.getElementsByTagName("tns:nome");        
+        Node nomeEvento = nomeEventoList.item(0);        
+        novoEvento.setNome(nomeEvento.getTextContent());
+        //
+        NodeList precoEventoList = documento.getElementsByTagName("tns:precoBilhete");        
+        Node precoEvento = precoEventoList.item(0);        
+        novoEvento.setPrecoBilhete(Double.parseDouble(precoEvento.getTextContent()));
+
+
+        System.out.println("Nome evento : " + novoEvento.getNome());
+        System.out.println("Preço evento : " + novoEvento.getPrecoBilhete() + "€");
         
     }
 
