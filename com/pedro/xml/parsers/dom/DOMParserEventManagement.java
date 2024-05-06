7a278f28a572dd557174e83206457abbb99cf038
albertobolina <60327260+albertobolina@users.noreply.github.com>
2024-05-06 17:17:15 +0000

    commit 20240506

--- com/pedro/xml/parsers/dom/DOMParserEventManagement.java
+++ com/pedro/xml/parsers/dom/DOMParserEventManagement.java
@@ -1,6 +1,7 @@
 package com.pedro.xml.parsers.dom;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -13,6 +14,8 @@ import org.xml.sax.SAXException;
 
 import com.pedro.xml.parsers.dto.*;
 
+// programa que lê um xml criado com base no "event.xsd", cria uma instancia de uma classe evento (DTO definida 
+// com a mesma estrutura do event.xsd) e faz display dos seus valores no ecran
 public class DOMParserEventManagement {
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
@@ -27,12 +30,38 @@ public class DOMParserEventManagement {
         NodeList nomeEventoList = documento.getElementsByTagName("tns:nome");        
         Node nomeEvento = nomeEventoList.item(0);        
         novoEvento.setNome(nomeEvento.getTextContent());
-        //
+        
+        
+        // preço do bilhete
         NodeList precoEventoList = documento.getElementsByTagName("tns:precoBilhete");        
         Node precoEvento = precoEventoList.item(0);        
         novoEvento.setPrecoBilhete(Double.parseDouble(precoEvento.getTextContent()));
 
+        // data inicio do evento
+        NodeList dataIniEventoList = documento.getElementsByTagName("tns:dataInicio");        
+        Node dataIniEvento = dataIniEventoList.item(0);    
+        novoEvento.setDataInicio(LocalDateTime.parse(dataIniEvento.getTextContent()));
 
+        // dados do organizador do evento
+        NodeList organizerEventoList = documento.getElementsByTagName("tns:organizador");        
+        Node organizerEvento = organizerEventoList.item(0);   
+        NodeList dadosOrganizer = organizerEvento.getChildNodes();
+        for (int i = 0; i < dadosOrganizer.getLength(); i++){
+            Node elementoDados = dadosOrganizer.item(i);
+            switch (elementoDados.getTextContent()) {
+                case "tns:nome":
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+
+        
+
+        // display dos valores
+        System.out.println("daata inicio : " + novoEvento.getDataInicio());
         System.out.println("Nome evento : " + novoEvento.getNome());
         System.out.println("Preço evento : " + novoEvento.getPrecoBilhete() + "€");
         
